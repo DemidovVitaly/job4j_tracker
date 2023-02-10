2eb340641b2eb426caabbe9f774346e4ecc56dce
DemidovVitaly <yademidov011@yandex.ru>
2023-02-10 07:28:34 +0300

    Правила Checkstyle [#504981 #389873] исправления

--- src/main/java/checkstyle/Broken.java
+++ src/main/java/checkstyle/Broken.java
@@ -2,20 +2,20 @@ package checkstyle;
 
 public class Broken {
     private int sizeOfEmpty = 10;
-    public String surname;
-    public static final String NEW_VALUE = "";
+    private String surname;
+    private static final String NEW_VALUE = "";
 
-    Broken() { }
+    public Broken() { }
 
-    void echo() { }
+    public void echo() { }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
         String name;
     }
 }
