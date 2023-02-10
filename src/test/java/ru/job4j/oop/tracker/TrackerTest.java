7569dbd12c13279985d065ab367fe21a6b3befea
DemidovVitaly <yademidov011@yandex.ru>
2023-02-10 07:17:33 +0300

    1.1. Создание объекта [#165452 #129634]

--- src/test/java/ru/job4j/oop/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/oop/tracker/TrackerTest.java
@@ -1,6 +1,8 @@
-package ru.job4j.tracker;
+package ru.job4j.oop.tracker;
 
 import org.junit.Test;
+import tracker.Item;
+import tracker.Tracker;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
