a5d180582b8f1318a87d754fdb7b3d387cf10341
DemidovVitaly <yademidov011@yandex.ru>
2023-03-23 06:48:07 +0300

    5. Tracker - хранилище [#396 #129657]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,16 +1,38 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
+    /* добавление заявок */
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
     }
 
+    /*получение списка всех заявок*/
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    /* получение списка по имени */
+    public Item[] findByName(String key) {
+        int num = 0;
+        Item[] rsl = new Item[size];
+
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                rsl[num++] = items[i];
+            }
+        }
+        return Arrays.copyOf(rsl, num);
+    }
+
+    /*  получение заявки по id */
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
