0f2b110d6c0c123a174aff0cb2c872910937dac0
Demidov011 <demidov.et78@gmail.com>
2023-03-30 20:49:28 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,8 +5,13 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
+        /* Вывод даты из урока 8*/
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         String currentDateTimeFormat = item.getCreated().format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+
+        /* Вывод даты из урока 10 переопределенным методом toString*/
+        System.out.println(item);
+
     }
 }
