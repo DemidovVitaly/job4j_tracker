8544085e7e6644bb9d4b8e6e2c941d23e8500089
Demidov011 <demidov.et78@gmail.com>
2023-04-09 21:58:43 +0300

    4.2. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,21 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+
+    @Override
+    public void drive() {
+        System.out.println("Bus is driving");
+    }
+
+    @Override
+    public void passengers(int passengers) {
+        System.out.println("Bus capacity is " + passengers);
+    }
+
+    @Override
+    public double refuel(double fuel) {
+        double coast = 59.60 * fuel;
+        System.out.println("Bus tank full coast is " + coast);
+        return coast;
+    }
+}
