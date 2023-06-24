5c056022531c01916a05da6b0ab934e93e16e123
DemidovVitaly <yademidov011@yandex.ru>
2023-06-24 17:05:45 +0300

    4. Сортировка номера [#221284 # [#221284 #129582]]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,6 +6,21 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        return left.compareTo(right);
+       /*
+       int rsl = 0;
+        String[] leftString = left.split("\\.", 3);
+        String[] rightString = right.split("\\.", 3);
+        for (int i = 0; i < Math.min(leftString.length, rightString.length); i++) {
+            rsl = Integer.compare(Integer.parseInt(leftString[i]), Integer.parseInt(rightString[i]));
+            if (rsl != 0) {
+                return rsl;
+            }
+        }
+        return rsl;
+        */
+        int leftNumber = Integer.parseInt(left.split("\\.", 2)[0]);
+        int rightNumber = Integer.parseInt(right.split("\\.", 2)[0]);
+        return Integer.compare(leftNumber, rightNumber);
+
     }
 }
